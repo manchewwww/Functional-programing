4bb310b07441bf55d787c9772cf9b54bb8ca7574
manchewww <georgimanchev2003@gmail.com>
2023-04-28 06:55:58 +0300

    za

--- Week 8/ForHomeTask1Week8.hs
+++ Week 8/ForHomeTask1Week8.hs
@@ -1,29 +1,25 @@
 main :: IO()
 main = do
-    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1 == [[1]]
-    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 -- == [[1, 2], [1, 3]]
-    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 -- == [[1, 2, 3], [1, 2, 4]]
-    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 -- == [[2,3],[2,4]]
-
+    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1 == [[1]]
+    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 -- == [[1, 2], [1, 3]]
+    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 -- == [[1, 2, 3], [1, 2, 4]]
+    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 -- == [[2,3],[2,4]]
+    print $ neighbors 1 [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 
+    
 type Node = Int
 type Graph = [(Node, [Node])]
 type Path = [Node]
 
+neighbors :: Node -> Graph -> [Node]
+neighbors n g =  concat [xs | (x, xs) <- g, x == n]
+
 simplePaths :: Graph -> Int -> Node -> [Path]
+simplePaths _ 0 n = [[n]]
 simplePaths graph k n
-  | not (elem n nodes) = error "Node not found in graph"
-  | otherwise = go k [n] 
-   where
+ | not (elem n nodes) = error "Node not found in graph"
+ | otherwise = 
+     where
       nodes = map fst graph
-      go 0 path = [path]
-      go m path = concat [go (m-1) (v:path) | v <- neighbors path]
-      neighbors path = case lookup (head path) graph of
-                          Just ns -> filter (`notElem` path) ns
-                          Nothing -> []
-
-
--- getChildren :: Graph -> Node -> [Node]
--- getChildren g n = head [children | (parent, children) <- g, parent == n]
 
 -- simplePaths :: Path -> Node -> Node -> [[Node]]
 -- simplePaths _ 0 n = [[n]]
