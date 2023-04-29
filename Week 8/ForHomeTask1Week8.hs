393f5a3a3a5f0741b06b439c795a6b207983e2ee
manchewww <georgimanchev2003@gmail.com>
2023-04-29 10:03:27 +0300

    an

--- Week 8/ForHomeTask1Week8.hs
+++ Week 8/ForHomeTask1Week8.hs
@@ -1,26 +1,23 @@
+import Data.List
 main :: IO()
 main = do
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1 == [[1]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 -- == [[1, 2], [1, 3]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 -- == [[1, 2, 3], [1, 2, 4]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 -- == [[2,3],[2,4]]
-    print $ neighbors 1 [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1 == [[1]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 == [[1, 2], [1, 3]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 == [[1, 2, 3], [1, 2, 4]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 == [[2,3],[2,4]]
     
 type Node = Int
 type Graph = [(Node, [Node])]
 type Path = [Node]
 
-neighbors :: Node -> Graph -> [Node]
-neighbors n g =  concat [xs | (x, xs) <- g, x == n]
+getChildren :: Graph -> Node -> [Node]
+getChildren g n = head [children | (parent, children) <- g, parent == n]
 
-simplePaths :: Graph -> Int -> Node -> [Path]
-simplePaths _ 0 n = [[n]]
-simplePaths graph k n
- | not (elem n nodes) = error "Node not found in graph"
- | otherwise = 
-     where
-      nodes = map fst graph
+isPath :: Graph -> Path -> Bool
+isPath g path = all (\ (parent, child) -> elem child (getChildren g parent))
+                    $ zip path (tail path)
 
--- simplePaths :: Path -> Node -> Node -> [[Node]]
--- simplePaths _ 0 n = [[n]]
--- simplePaths graph k n = 
+simplePaths :: Graph -> Node -> Node -> [Path]
+simplePaths graph k n = [xs | xs <- subsequences nodes, length xs == k + 1 && isPath graph xs, head xs == n]
+ where
+     nodes = map fst graph
