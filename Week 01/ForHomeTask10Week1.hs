98736cb274cb499ac9f5f2b173e28a6acf9a75d5
manchewww <georgimanchev2003@gmail.com>
2023-06-18 11:28:42 +0300

    das

--- Week 01/ForHomeTask10Week1.hs
+++ Week 01/ForHomeTask10Week1.hs
@@ -1,7 +1,7 @@
 main :: IO()
 main = do
-    print $ finalGrade 3 4 4 4.25 4.50 3.75 4.25 5 4.25 == 4.34
-    print $ finalGrade 6 6 6 4.50 5 4.50 4.75 5 4.75    == 4.95
+    print $ finalGrade 6 6 5 5.5 6 4.75 4.75 5.75 4.75 -- == 4.34
+    print $ finalGrade 6 6 4 5 6 6 4.75 5.5 5.37 --   == 4.95
     print $ finalGrade 6 0 4 6 6 5 4.75 6 4.75          == 5.14
     print $ finalGrade 4.25 0 3 2 0 0 0 0 0             == 2
     print $ finalGrade 5.50 6 6 6 5.50 5.25 4 5.50 4    == 5.05
